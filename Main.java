2eb86a1dc1c261e678c3cdec663bcbf4d121d3fd
KOTTAGE N.V.H <95292564+NOWEN-KOTTAGE@users.noreply.github.com>
2022-01-08 07:48:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+//Fig. 2.1: Welcome 1. java
+//Text-printing program
+
+public class Main {
+	//main method begins execution of Java application
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java programming!");
+	}//end method main
+}//end class Main
